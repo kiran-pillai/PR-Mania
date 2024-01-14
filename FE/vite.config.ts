be579abee0c9cfa02b68f80c37a30310d96ea7c1
Kiran Pillai <kirpilai_deloitte>
2024-01-14 16:14:15 -0500

    hmr achieved in docker

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // Here
+    host: true,
     strictPort: true,
     port: 3001,
   },
