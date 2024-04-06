c9fbaa7a9411a0d21aa996e7409c128dc0c572cc
kiran-pillai <kirpilai_deloitte>
2024-04-06 00:45:51 -0400

    docker working for ui again

--- FE/src/routes/-components/Layout.tsx
+++ FE/src/routes/-components/Layout.tsx
@@ -1,16 +1,12 @@
-import { ModeToggle } from '@/components/mode-toggle';
-import { Button } from '@/components/ui/button';
-import { useAuthContext } from '@/context/authContext';
-import { urlToURI } from '@/urlHandler';
-import { Link, Outlet, useNavigate } from '@tanstack/react-router';
+import { Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import { MessageSquareMore } from 'lucide-react';
 import TopBar from './Topbar';
 import Sidebar from './Sidebar';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const Layout = () => {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="flex flex-col">
         <TopBar />
         <Sidebar>
@@ -19,7 +15,7 @@ const Layout = () => {
       </div>
       <hr />
       <TanStackRouterDevtools />
-    </>
+    </QueryClientProvider>
   );
 };
 export default Layout;
